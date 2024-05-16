84a08a5734e0227c3a9d823205cd6922c337245b
joaovictorlinhares <joao.linhares.dev@gmail.com>
2024-05-16 10:20:42 -0300

    Adição do atualizar e deletar de promoção.

--- src/main/java/br/com/promocoes_api/api/controllers/PromocaoController.java
+++ src/main/java/br/com/promocoes_api/api/controllers/PromocaoController.java
@@ -3,6 +3,8 @@ package br.com.promocoes_api.api.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.promocoes_api.api.dto.produto.ProdutoRequestDTO;
+import br.com.promocoes_api.api.dto.produto.ProdutoResponseDTO;
 import br.com.promocoes_api.api.dto.promocao.PromocaoRequestDTO;
 import br.com.promocoes_api.api.dto.promocao.PromocaoResponseDTO;
 import br.com.promocoes_api.api.services.PromocaoService;
@@ -11,10 +13,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -39,6 +44,16 @@ public class PromocaoController {
         PromocaoResponseDTO promocaoResponse = this.promocaoService.criarPromocao(promocaoRequest);    
         return ResponseEntity.status(HttpStatus.CREATED).body(promocaoResponse);
     }
-    
-    
+
+    @PutMapping("/{idPromocao}/Atualizar")
+    public ResponseEntity<PromocaoResponseDTO> atualizarPromocao(@PathVariable Long idPromocao, @RequestBody PromocaoRequestDTO promocao) {
+        PromocaoResponseDTO promocaoResponse = this.promocaoService.atualizarPromocao(idPromocao, promocao);
+        return ResponseEntity.status(HttpStatus.OK).body(promocaoResponse);
+    }
+
+    @DeleteMapping("/{idPromocao}/Deletar")
+    public ResponseEntity<Void> deletarPromocao(@PathVariable Long idPromocao) {
+        this.promocaoService.deletePromocao(idPromocao);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/br/com/promocoes_api/api/services/PromocaoService.java
+++ src/main/java/br/com/promocoes_api/api/services/PromocaoService.java
@@ -3,9 +3,13 @@ package br.com.promocoes_api.api.services;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.promocoes_api.api.dto.produto.ProdutoRequestDTO;
+import br.com.promocoes_api.api.dto.produto.ProdutoResponseDTO;
 import br.com.promocoes_api.api.dto.promocao.PromocaoRequestDTO;
 import br.com.promocoes_api.api.dto.promocao.PromocaoResponseDTO;
 import br.com.promocoes_api.api.exceptions.PromocaoException.PromocaoNotFoundException;
+import br.com.promocoes_api.api.exceptions.produtoException.ProdutoNotFoundException;
+import br.com.promocoes_api.api.models.Produto;
 import br.com.promocoes_api.api.models.Promocao;
 import br.com.promocoes_api.api.repositorys.PromocaoRepository;
 import lombok.RequiredArgsConstructor;
@@ -32,4 +36,20 @@ public class PromocaoService {
         return new PromocaoResponseDTO(novaPromocao);
     }
 
+    public PromocaoResponseDTO atualizarPromocao(Long idPromocao, PromocaoRequestDTO promocaoRequestDTO) {
+        Promocao existePromocao = this.promocaoRepository.findById(idPromocao).orElseThrow(() -> new ProdutoNotFoundException("O Id " + idPromocao + " nao existe."));
+
+        existePromocao.setTitulo(promocaoRequestDTO.titulo());
+        existePromocao.setDesconto(promocaoRequestDTO.desconto());
+        existePromocao.setDataInicioPromo(promocaoRequestDTO.dataInicioPromo());
+        existePromocao.setDataFimPromo(promocaoRequestDTO.dataFimPromo());
+
+        promocaoRepository.save(existePromocao);
+        return new PromocaoResponseDTO(existePromocao);
+    }
+
+    public void deletePromocao(Long idPromocao) {
+        Promocao existeProduto = promocaoRepository.findById(idPromocao).orElseThrow(() -> new ProdutoNotFoundException("O Id " + idPromocao + " nao existe."));
+        promocaoRepository.delete(existeProduto);
+    }
 }
