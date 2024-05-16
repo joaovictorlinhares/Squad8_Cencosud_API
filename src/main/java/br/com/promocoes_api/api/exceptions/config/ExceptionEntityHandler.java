cfe5c2c8b1b7eb86ff1b48ab4a7252e73d8c98f1
joaovictorlinhares <joao.linhares.dev@gmail.com>
2024-05-16 10:18:16 -0300

    Ajuste na config das exception

--- src/main/java/br/com/promocoes_api/api/exceptions/config/ExceptionEntityHandler.java
+++ src/main/java/br/com/promocoes_api/api/exceptions/config/ExceptionEntityHandler.java
@@ -1,9 +1,12 @@
 package br.com.promocoes_api.api.exceptions.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.com.promocoes_api.api.exceptions.PromocaoException.PromocaoNotFoundException;
+import br.com.promocoes_api.api.exceptions.produtoException.ProdutoNotFoundException;
 import br.com.promocoes_api.api.exceptions.secaoException.SecaoNotFoundException;
 
 @ControllerAdvice
@@ -11,6 +14,16 @@ public class ExceptionEntityHandler {
 
     @ExceptionHandler(SecaoNotFoundException.class)
     public ResponseEntity handlerEventNotFound(SecaoNotFoundException exception) {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    };
+
+    @ExceptionHandler(ProdutoNotFoundException.class)
+    public ResponseEntity handlerEventNotFound(ProdutoNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    };
+
+    @ExceptionHandler(PromocaoNotFoundException.class)
+    public ResponseEntity handlerEventNotFound(PromocaoNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     };
 }
