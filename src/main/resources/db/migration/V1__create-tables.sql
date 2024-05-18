05b2d1997a21a19e6eea2ec169ae0fd9fa2cbd63
joaovictorlinhares <joao.linhares.dev@gmail.com>
2024-05-18 15:18:48 -0300

    Ajuste no SQL

--- src/main/resources/db/migration/V1__create-tables.sql
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -3,6 +3,14 @@ CREATE TABLE secoes (
   nome VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE promocoes (
+  id SERIAL PRIMARY KEY,
+  titulo VARCHAR(100) NOT NULL,
+  desconto DECIMAL(5,2) NOT NULL,
+  data_inicio_promo DATE NOT NULL,
+  data_fim_promo DATE NOT NULL
+);
+
 CREATE TABLE produtos (
   id SERIAL PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
@@ -12,14 +20,6 @@ CREATE TABLE produtos (
   data_ultima_atualizacao_preco DATE NOT NULL,
   marca VARCHAR(100) NOT NULL,
   categoria VARCHAR(100) NOT NULL,
-  id_secao INT NOT NULL REFERENCES secoes(id)
-);
-
-CREATE TABLE promocoes (
-  id SERIAL PRIMARY KEY,
-  titulo VARCHAR(100) NOT NULL,
-  desconto DECIMAL(5,2) NOT NULL,
-  data_inicio_promo DATE NOT NULL,
-  data_fim_promo DATE NOT NULL,
-  id_produto_fk INT REFERENCES produtos(id)
+  id_secao INT NOT NULL REFERENCES secoes(id),
+  id_promocao INT REFERENCES promocoes(id)
 );
